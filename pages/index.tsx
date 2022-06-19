dbc26c15a07e7e8c2dc639e5582f35b7b1939d0a
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2022-06-19 20:01:02 +0300

    added react-query spotify authentication complete with auth0 corrected added .env to git ignore completed working on player a few issues left

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,21 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { Flex, Text, chakra } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { FlexColCenterCenter, FlexRowCenterCenter } from '../utils/FlexConfigs'
 
 const Home: NextPage = () => {
-  const {loginWithPopup} = useAuth0()
+  const {loginWithPopup, user ,isLoading, error} = useAuth0()
+  
+  const {push} = useRouter()
+    useEffect(()=>{
+      if(!isLoading){
+        if(typeof user !== "undefined"){
+          push("/player")
+        }
+      }
+    }, [, isLoading, error, user])
   return (
     <Flex width="100vw" backgroundImage={"/unsplash_images/music_waves.png"} backgroundSize="cover" backgroundRepeat={"no-repeat"}  height="100vh" {...FlexColCenterCenter}  >
           <Flex width="80%" height={"80%"} borderRadius="8px" backdropFilter={"auto"} background="transparent" backdropBlur={"24px"} overflow="hidden" >
@@ -16,12 +27,28 @@ const Home: NextPage = () => {
               </chakra.p>
             </Flex>
             <Flex {...FlexColCenterCenter} w="50%" h="100%"  >
-                <chakra.button borderRadius={"999px"} onClick={loginWithPopup}  overflow="hidden" display={"flex"} flexDir="row" alignItems="center" justifyContent={"space-between"} bg="none"  _active={{border: "1px solid white", scale: "0.95"}} _pressed={{outline: "1px solid white", scale: "0.95"}} padding="5px 10px"  >
+                {(!isLoading  ) && <chakra.button borderRadius={"999px"} onClick={loginWithPopup}  overflow="hidden" display={"flex"} flexDir="row" alignItems="center" justifyContent={"space-between"} bg="none"  _active={{border: "1px solid white", scale: "0.95"}} _pressed={{outline: "1px solid white", scale: "0.95"}} padding="5px 10px"  >
                         <Image src="/icons/spotify.svg" height="50px"  width="50px"  />
                         <chakra.p marginLeft={"20px"} >
                           Sign In
                         </chakra.p>
-                </chakra.button>
+                </chakra.button>}
+                {
+                  typeof error !== "undefined" && 
+                    <Flex  >
+                        <chakra.p>
+                          An error occured
+                        </chakra.p>
+                    </Flex>
+                  
+                }
+                {
+                  isLoading && <Flex>
+                      <chakra.p>
+                        Loading...
+                      </chakra.p>
+                  </Flex>
+                }
             </Flex>
           </Flex>      
     </Flex>
